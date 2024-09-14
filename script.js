e3e887927129e49a1ddccd34529224fa3b420e8a
Ryn-1729 <oparyanff4781@gmail.com>
2024-09-14 08:29:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ function myFunction(){
+    alert("Home")
+ }
+ function myFunction2(){
+    alert("Gift")
+ }
+ function myFunction3(){
+    alert("Order")
+ }
+ function myFunction4(){
+    alert("Pay")
+ }
+ function myFunction5(){
+    alert("Store")
+ }
+ function myFunction6(){
+    window.location.href = "https://www.starbucks.in/rewards";
+ }
